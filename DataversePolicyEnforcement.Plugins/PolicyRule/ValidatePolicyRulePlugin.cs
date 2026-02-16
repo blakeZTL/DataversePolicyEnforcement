459faf7024c17b7dce77eca71dcc59e61a92c717
Blake Bradford <james.b.bradford@faa.gov>
2026-02-16 12:51:30 -0500

    Add ValidatePolicyRulePlugin and ClickOnce config updates
    Introduced ValidatePolicyRulePlugin class as a stub under PolicyRule and updated the project file to include it. Enhanced the .csproj with ClickOnce deployment and bootstrapper settings. Removed the empty PolicyRule folder entry since it now contains a source file.

---
+++ DataversePolicyEnforcement.Plugins/PolicyRule/ValidatePolicyRulePlugin.cs
@@ -0,0 +1,18 @@
+﻿namespace DataversePolicyEnforcement.Plugins.PolicyRule
+{
+    public class ValidatePolicyRulePlugin : PluginBase
+    {
+        public ValidatePolicyRulePlugin()
+            : base(typeof(ValidatePolicyRulePlugin))
+        {
+            // Not Implemented
+        }
+
+        protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
+        {
+            var context = localPluginContext.PluginExecutionContext;
+            var systemService = localPluginContext.SystemUserService;
+            var tracer = localPluginContext.TracingService;
+        }
+    }
+}
